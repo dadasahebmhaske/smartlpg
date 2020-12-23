b345f652416765b34ec7ba8b394f16f0c7554f9d
dmask <dadasaheb41@gmail.com>
2020-12-23 15:24:58 +0530

    change password compltee

--- src/app/features/setting/change-password/change-password.component.ts
+++ src/app/features/setting/change-password/change-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '@app/core/custom-services/app.service';
 
 @Component({
   selector: 'sa-change-password',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent implements OnInit {
-
-  constructor() { }
+  public cpInfo: any = {};
+  public pass: any = {};
+  public loaderbtn: boolean = true;
+  public errorMsg: boolean = false;
+  constructor(private appService: AppService) { }
 
   ngOnInit() {
+    this.appService.getAppData().subscribe(data => { this.cpInfo = data; });
+  }
+  checkingCurrentPassword() {
+    this.errorMsg = this.pass.CurrentPassword != this.cpInfo.Password ? true : false;
   }
 
 }
